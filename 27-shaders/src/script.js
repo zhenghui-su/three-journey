32ce6c660f8277a5ffbe39ad1fe05a15dec84b6c
zhenghui-su <343196323@qq.com>
2025-04-15 23:10:53 +0800

    ✨ feat: 完成着色器基础章节

--- 27-shaders/src/script.js
+++ 27-shaders/src/script.js
@@ -20,6 +20,7 @@ const scene = new THREE.Scene();
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
+const flagTexure = textureLoader.load('/textures/flag-french.jpg');
 
 /**
  * Test mesh
@@ -27,12 +28,50 @@ const textureLoader = new THREE.TextureLoader();
 // Geometry
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
 
+const count = geometry.attributes.position.count;
+const randoms = new Float32Array(count);
+
+for (let i = 0; i < count; i++) {
+	randoms[i] = Math.random();
+}
+geometry.setAttribute('aRandom', new THREE.BufferAttribute(randoms, 1));
+
 // Material
-const material = new THREE.RawShaderMaterial({
+// const material = new THREE.RawShaderMaterial({
+// 	vertexShader: testVertexShader,
+// 	fragmentShader: testFragmentShader,
+// 	transparent: true,
+// 	uniforms: {
+// 		uFrequency: { value: new THREE.Vector2(10, 5) },
+// 		uTime: { value: 0 },
+// 		uColor: { value: new THREE.Color('orange') },
+// 		uTexture: { value: flagTexure },
+// 	},
+// });
+const material = new THREE.ShaderMaterial({
 	vertexShader: testVertexShader,
 	fragmentShader: testFragmentShader,
 	transparent: true,
+	uniforms: {
+		uFrequency: { value: new THREE.Vector2(10, 5) },
+		uTime: { value: 0 },
+		uColor: { value: new THREE.Color('orange') },
+		uTexture: { value: flagTexure },
+	},
 });
+
+gui
+	.add(material.uniforms.uFrequency.value, 'x')
+	.min(0)
+	.max(20)
+	.step(0.01)
+	.name('frequencyX');
+gui
+	.add(material.uniforms.uFrequency.value, 'y')
+	.min(0)
+	.max(20)
+	.step(0.01)
+	.name('frequencyY');
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material);
@@ -93,6 +132,8 @@ const clock = new THREE.Clock();
 
 const tick = () => {
 	const elapsedTime = clock.getElapsedTime();
+
+	material.uniforms.uTime.value = elapsedTime;
 
 	// Update controls
 	controls.update();
--- 27-shaders/src/shaders/test/fragment.glsl
+++ 27-shaders/src/shaders/test/fragment.glsl
@@ -1,5 +1,14 @@
-precision mediump float;
+// precision mediump float;
+
+uniform vec3 uColor;
+uniform sampler2D uTexture;
+
+varying vec2 vUv;
+varying float vElevation;
+// varying float vRandom;
 
 void main() {
-  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  vec4 textureColor = texture2D(uTexture, vUv);
+  textureColor.rgb *= vElevation * 2.0 + 0.5;
+  gl_FragColor = textureColor;
 }
